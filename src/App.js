7c132175317e0bacb7b4c5bb26327917990f378f
Schmidt-Aaron <aschmidt808@gmail.com>
2017-12-29 12:53:24 -0800

    added focus on load to search

--- src/App.js
+++ src/App.js
@@ -12,17 +12,34 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
-const Search = ({ value, onChange, onSubmit, children }) =>
-  <form onSubmit={onSubmit}>
-    <input 
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+class Search extends Component {
+  componentDidMount() {
+    this.input.focus();
+  }
+  
+  render() {
+    const { 
+      value, 
+      onChange, 
+      onSubmit, 
+      children 
+    } = this.props;
+    
+    return (
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
