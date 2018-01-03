a77253a63c465023be64c6534dd8cacd786271d5
Schmidt-Aaron <aschmidt808@gmail.com>
2018-01-02 16:58:38 -0800

    refactored setState

--- src/App.js
+++ src/App.js
@@ -215,6 +215,27 @@ const Sort = ({
   );
 }
 
+const updateSearchTopStoriesState = (hits, page) => {
+  const { searchKey, results } = prevState;
+
+  const oldHits = results && results[searchKey]
+  ? results[searchKey].hits
+  : [];
+  
+  const updatedHits = [
+    ...oldHits,
+    ...hits
+  ];
+
+  return {
+    results: { 
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    },
+    isLoading: false
+  };
+}
+
 class App extends Component {
   
   constructor(props) {
@@ -242,26 +263,9 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page} = result;
-    const { searchKey, results } = this.state;
-
-    const oldHits = results && results[searchKey]
-    ? results[searchKey].hits
-    : [];
-
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
-
-    this.setState({ 
-      results: { 
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      },
-      isLoading: false
-    });
-  }
-
+    this.setState(updateSearchTopStoriesState(hits, page));
+  }       
+  
   fetchSearchTopStories(searchTerm, page = 0) {
     this.setState({ isLoading: true });
     
